10c3c0c3c91bb5236168193eee13950016f80de6
jome-miniStack <jome.2025.work@gmail.com>
2025-04-10 17:42:40 +0800

    fix .envs

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd())
+  const env = loadEnv(mode, path.resolve(__dirname, '.envs'))
   return {
     plugins: [
       TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
