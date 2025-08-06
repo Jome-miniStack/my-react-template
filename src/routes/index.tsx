7168e21fc265889dfa35582d8950d435958e6215
jome <jome.2025.work@gmail.com>
2025-08-07 00:45:17 +0800

    remove init code

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,3 @@
-import { Button } from '@/components/ui/button'
-import { BASE_API_URL } from '@/constants/env'
-import { useCounter } from '@/store/useCounter'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
@@ -8,15 +5,5 @@ export const Route = createFileRoute('/')({
 })
 
 function Index() {
-  const { count, incr } = useCounter()
-  return (
-    <div className="flex items-center">
-      <Button className="rounded-full bg-blue-500" onClick={incr}>
-        Add {count}
-      </Button>
-      <span>
-        {import.meta.env.MODE} BASE_API_URL: {BASE_API_URL}
-      </span>
-    </div>
-  )
+  return <></>
 }
